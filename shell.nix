522dedccd78f156fbcb185d354614527e874fa53
alexkharl <sasha_xarlamov@mail.ru>
2024-10-10 00:25:53 +0300

    Repo:Доб-ие инфра-структуры

---
+++ shell.nix
@@ -0,0 +1,32 @@
+
+{ sources ? import ./npins }:
+let
+  pkgs = import sources.nixpkgs { config = {}; overlays = []; };
+  pythonPackages = pkgs.python3Packages;
+in pkgs.mkShell rec {
+  name = "impurePythonEnv";
+  venvDir = "./.venv";
+  buildInputs = with pkgs; [
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    pythonPackages.python
+
+    # This executes some shell code to initialize a venv in $venvDir before
+    # dropping into the shell
+    pythonPackages.venvShellHook
+  ];
+
+  # Run this command, only after creating the virtual environment
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    pip install -r requirements.txt
+  '';
+
+  # Now we can execute any commands within the virtual environment.
+  # This is optional and can be left out to run pip manually.
+  postShellHook = ''
+    # allow pip to install wheels
+    unset SOURCE_DATE_EPOCH
+  '';
+
+}
