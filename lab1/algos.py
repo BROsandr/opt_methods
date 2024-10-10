39fa0e23fcec039155b308605320560fbb3c2cbe
brosandr <sasha_xarlamov@mail.ru>
2024-10-10 15:20:05 +0300

    Feat(algos):Доб-ие шаблона bitwise_search

--- lab1/algos.py
+++ lab1/algos.py
@@ -16,3 +16,21 @@ def brute_force(f: Callable[[Any], Any], a, b, eps)->Point:
   ind_min = np.argmin(y)
 
   return Point(x=x[ind_min], y=y[ind_min])
+
+def bitwise_search(f: Callable[[Any], Any], a, b, eps)->Point:
+  assert a <= b
+  assert eps > 0
+
+  delta1 = eps * (0.25 / 0.1)
+
+  delta1_x = np.arange(a, b + delta1, delta1)
+  delta1_y = [f(delta1_x[0])]
+
+  for f_curr, f_next in zip((f(xi) for xi in delta1_y + delta1_x[1:-1]),
+                            (f(xi) for xi in delta1_x[1:])):
+    delta1_y.append(f_next)
+    if f_curr <= f_next: break
+
+  for i, x in enumerate(delta1_y):
+    points = dict(zip(round(delta1_x, delta1_y))
+  delta2 = delta1 / 4
