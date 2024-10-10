f3e4546010419646f8ddf82c08aec642f39b168e
brosandr <sasha_xarlamov@mail.ru>
2024-10-10 19:05:05 +0300

    Feat(algos):Допиливание bitwise_search

--- lab1/algos.py
+++ lab1/algos.py
@@ -17,20 +17,40 @@ def brute_force(f: Callable[[Any], Any], a, b, eps)->Point:
 
   return Point(x=x[ind_min], y=y[ind_min])
 
-def bitwise_search(f: Callable[[Any], Any], a, b, eps)->Point:
+def bitwise_search(f: Callable[[Any], Any], a, b, eps, get_init_delta=lambda: 0.25)->Point:
   assert a <= b
   assert eps > 0
 
-  delta1 = eps * (0.25 / 0.1)
+  delta1 = get_init_delta()
+  assert get_init_delta > eps
+  points = {}
 
-  delta1_x = np.arange(a, b + delta1, delta1)
-  delta1_y = [f(delta1_x[0])]
+  while True:
+    delta1_x = np.arange(a, b + delta1, delta1)
 
-  for f_curr, f_next in zip((f(xi) for xi in delta1_y + delta1_x[1:-1]),
-                            (f(xi) for xi in delta1_x[1:])):
-    delta1_y.append(f_next)
-    if f_curr <= f_next: break
+    def get_f(x):
+      if x in points:
+        return points[x]
+      else:
+        return f(x)
 
-  for i, x in enumerate(delta1_y):
-    points = dict(zip(round(delta1_x, delta1_y))
-  delta2 = delta1 / 4
+    delta1_y = [get_f(delta1_x[0])]
+
+    endpoint_min = True
+    for f_curr, f_next in zip((get_f(xi) for xi in delta1_y + delta1_x[1:-1]),
+                              (get_f(xi) for xi in delta1_x[1:])):
+      delta1_y.append(f_next)
+      if f_curr <= f_next:
+        endpoint_min = False
+        break
+
+    if endpoint_min:
+      return Point(x=delta1_x[-1], y=delta1_y[-1])
+
+    if delta1 <= eps:
+      return Point(x=delta1_x[-2], y=delta1_y[-2])
+
+    points.update(dict(zip(delta1_x, delta1_y)))
+
+    delta1 = -delta1 / 4
+    a, b = b, a
