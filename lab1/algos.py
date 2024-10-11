e982269a04df8ae62f83664bfba23eb0b788a7d1
BROsandr <53278658+BROsandr@users.noreply.github.com>
2024-10-11 12:23:16 +0300

    Fea:Добавление bitwise_search

--- lab1/algos.py
+++ lab1/algos.py
@@ -1,7 +1,7 @@
 import numpy as np
 from utils import Point
 from typing import Callable, Any
-
+import itertools
 import math
 
 def brute_force(f: Callable[[Any], Any], a, b, eps)->Point:
@@ -16,3 +16,39 @@ def brute_force(f: Callable[[Any], Any], a, b, eps)->Point:
   ind_min = np.argmin(y)
 
   return Point(x=x[ind_min], y=y[ind_min])
+
+def bitwise_search(f: Callable[[Any], Any], a, b, eps, get_init_delta=lambda: 0.25)->Point:
+  assert a <= b
+  assert eps > 0
+
+  delta1 = get_init_delta()
+  assert delta1 > eps
+  points = {}
+
+  while True:
+    def get_f(x):
+      if x in points:
+        return points[x]
+      else:
+        return f(x)
+
+    f_curr = get_f(a)
+    points[a] = f_curr
+
+    x = a + delta1
+
+    while x <= b:
+      f_next = get_f(x)
+      points[x] = f_next
+      if f_curr <= f_next: break
+      f_curr = f_next
+      x += delta1
+
+    if x > b:
+      return Point(x=b, y=f_curr)
+
+    if abs(delta1) <= eps:
+      return Point(x=x-delta1, y=f_curr)
+
+    delta1 = -delta1 / 4
+    a = x
--- lab1/test.py
+++ lab1/test.py
@@ -55,5 +55,86 @@ class TestBruteFroce(unittest.TestCase):
         self.assertTrue(math.isclose(expected_points[i].x, actual_points[i].x))
         self.assertTrue(math.isclose(a=expected_points[i].y, b=actual_points[i].y, abs_tol=1e-2))
 
+class TestBitwiseSearch(unittest.TestCase):
+  @staticmethod
+  def f_lecture(x):
+    return x**4 + math.exp(-x)
+
+  def test_lecture_min(self):
+    f = self.f_lecture
+    real_xy = Point(x=0.5825, y=0.66750)
+    eps = 0.1
+
+    actual_xy = bitwise_search(f=f, a=0, b=1, eps=eps)
+
+    atol = get_y_abs_tol(f=f, x=real_xy.x, eps=eps)
+
+    self.assertTrue(math.isclose(a=actual_xy.x, b=real_xy.x, abs_tol=eps))
+    self.assertTrue(math.isclose(a=actual_xy.y, b=real_xy.y, abs_tol=atol))
+
+  def test_lecture_all_points(self):
+    expected_points = [
+      Point(x=0.0000, y=1.000),
+      Point(x=0.2500, y=0.783),
+      Point(x=0.5000, y=0.669),
+      Point(x=0.7500, y=0.789),
+      Point(x=0.6875, y=0.726),
+      Point(x=0.6250, y=0.688),
+      Point(x=0.5625, y=0.670),
+      Point(x=0.4375, y=0.682),
+    ]
+
+    eps = 0.1
+    log_points = LogPointsWrap(self.f_lecture)
+    bitwise_search(f=log_points, a=0, b=1, eps=eps)
+    actual_points = log_points.points
+
+    self.assertEqual(len(expected_points), len(actual_points))
+
+    for i in range(len(expected_points)):
+      with self.subTest(i=i):
+        self.assertTrue(math.isclose(expected_points[i].x, actual_points[i].x))
+        self.assertTrue(math.isclose(a=expected_points[i].y, b=actual_points[i].y, abs_tol=1e-3))
+
+  def test_endpoint(self):
+    f = lambda x: -x
+    real_xy = Point(x=1, y=-1)
+    eps = 0.1
+
+    log_points = LogPointsWrap(f)
+    actual_xy = bitwise_search(f=log_points, a=0, b=1, eps=eps)
+
+    self.assertEqual(actual_xy.x, real_xy.x)
+    self.assertEqual(actual_xy.y, real_xy.y)
+
+    expected_points = [
+      Point(x=0.00, y=-0.00),
+      Point(x=0.25, y=-0.25),
+      Point(x=0.50, y=-0.50),
+      Point(x=0.75, y=-0.75),
+      Point(x=1.00, y=-1.00),
+    ]
+
+    actual_points = log_points.points
+
+    self.assertEqual(len(expected_points), len(actual_points))
+
+    for i in range(len(expected_points)):
+      with self.subTest(i=i):
+        self.assertEqual(expected_points[i].x, actual_points[i].x)
+        self.assertEqual(expected_points[i].y, actual_points[i].y)
+
+  def test_inf(self):
+    f = self.f_lecture
+    real_xy = Point(x=0.5825, y=0.66750)
+    eps = 0.1
+
+    actual_xy = bitwise_search(f=f, a=0, b=math.inf, eps=eps)
+
+    atol = get_y_abs_tol(f=f, x=real_xy.x, eps=eps)
+
+    self.assertTrue(math.isclose(a=actual_xy.x, b=real_xy.x, abs_tol=eps))
+    self.assertTrue(math.isclose(a=actual_xy.y, b=real_xy.y, abs_tol=atol))
+
 if __name__ == '__main__':
     unittest.main()
