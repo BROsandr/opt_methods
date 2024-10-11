fb26d6463330b08084afb93623a214f942b9e66d
alexkharl <sasha_xarlamov@mail.ru>
2024-10-11 23:53:58 +0300

    Feat:Доб-ие parabola(...)

--- lab1/test.py
+++ lab1/test.py
@@ -177,5 +177,39 @@ class TestDichotomy(unittest.TestCase):
         self.assertTrue(math.isclose(a=expected_points[i].x, b=actual_points[i].x, abs_tol=1e-3))
         self.assertTrue(math.isclose(a=expected_points[i].y, b=actual_points[i].y, abs_tol=1e-3))
 
+class TestParabola(unittest.TestCase):
+  def test_lecture_min(self):
+    f = f_lecture
+    eps = 0.1
+
+    actual_xy = parabola(f=f, a=0, b=1, eps=eps)
+
+    atol = get_y_abs_tol(f=f, x=LECTURE_MIN.x, eps=eps)
+
+    self.assertTrue(math.isclose(a=actual_xy.x, b=LECTURE_MIN.x, abs_tol=eps))
+    self.assertTrue(math.isclose(a=actual_xy.y, b=LECTURE_MIN.y, abs_tol=atol))
+
+  def test_lecture_all_points(self):
+    expected_points = [
+      Point(x=0.2500, y=0.7827),
+      Point(x=0.5000, y=0.6690),
+      Point(x=0.7500, y=0.7888),
+      Point(x=0.4968, y=0.6694),
+      Point(x=0.5224, y=0.6676),
+      Point(x=0.5248, y=None),
+    ]
+
+    eps = 0.1
+    log_points = LogPointsWrap(f_lecture)
+    brute_force(f=log_points, a=0, b=1, eps=eps)
+    actual_points = log_points.points
+
+    self.assertEqual(len(expected_points), len(actual_points))
+
+    for i in range(len(expected_points)):
+      with self.subTest(i=i):
+        self.assertTrue(math.isclose(expected_points[i].x, actual_points[i].x))
+        self.assertTrue(math.isclose(a=expected_points[i].y, b=actual_points[i].y, abs_tol=1e-4))
+
 if __name__ == '__main__':
     unittest.main()
