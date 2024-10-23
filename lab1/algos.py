8f80783454ff0c3e7ca7f04d9b003b8a6b00e29b
alexkharl <sasha_xarlamov@mail.ru>
2024-10-24 01:06:33 +0300

    Fix(algos):Испр-ие выч-ия x1,x2 в golden_ratio

--- lab1/algos.py
+++ lab1/algos.py
@@ -92,19 +92,19 @@ def golden_ratio(f: Callable[[Any], Any], a, b, eps)->Point:
   tau = (math.sqrt(5) - 1) / 2
   eps_n = (b - a) / 2
 
-  while eps_n <= eps:
+  while eps_n > eps:
     if y1 <= y2:
       b = x2
       x2 = x1
       y2 = y1
-      x1 = b - tau * (b - a)
+      x1 = a + b - x2
 
       y1 = f(x1)
     else:
       a = x1
       x1 = x2
       y1 = y2
-      x2 = b - tau * (b - a)
+      x2 = a + b - x1
 
       y2 = f(x2)
 
