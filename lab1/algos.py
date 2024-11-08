b1e2aa77df0dc5986fbac086b18743478c95aca9
alexkharl <sasha_xarlamov@mail.ru>
2024-11-08 17:33:45 +0300

    Revert "Fix(lab1/alogs/poly):Доб-ие abs(delta)"
    This reverts commit 037d2f1f7a00c5791781bdc887f3425cadbc7955.

--- lab1/algos.py
+++ lab1/algos.py
@@ -289,7 +289,7 @@ def polygonal_chain(f: Callable[[Any], Any], a, b, eps, get_L):
     star_pair = pairs.pop(np.argmin([pair.y for pair in pairs]))
     f_star = f(star_pair.x)
     delta = 1 / (2*L) * (f_star - star_pair.y)
-    if 2 * L * abs(delta) <= eps:
+    if 2 * L * delta <= eps:
       return Point(x=star_pair.x, y=f_star)
 
     p = 1 / 2 * (f_star + star_pair.y)
